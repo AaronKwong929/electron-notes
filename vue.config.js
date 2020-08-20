9a4b192545a9c35078b32c13d7fbfaa1a4cd05c6
Aaronkwong929 <AaronKwong929@gmail.com>
2020-08-20 23:57:00 +0800

    [feat]initial commit

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const resolve = dir => {
+    return path.join(__dirname, dir);
+};
+
+module.exports = {
+    css: {
+        loaderOptions: {
+            sass: {
+                prependData: `@import "@/static/style/index.scss";`
+            }
+        },
+        sourceMap: false
+    },
+
+    parallel: true,
+
+    chainWebpack: config => {
+        config.resolve.alias
+            .set('@assets', resolve('src/assets'))
+            .set('@views', resolve('src/views'))
+            .set('@components', resolve('src/components'))
+            .set('@router', resolve('src/router'))
+            .set('@/', resolve('src'))
+            .set('@static', resolve('src/static'))
+            .set('@utils', resolve('src/utils'))
+            .set('@mixins', resolve('src/mixins'))
+            .set('@filters', resolve('src/filters'));
+    }
+};
